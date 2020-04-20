9b69a894e56d3aea13a6c3fc3af83ca6c379d857
svgrossarth <svgrossarth@ucdavis.edu>
2020-04-20 01:04:08 -0700

    can save screen as image

--- ios_index_computer_vision/ios_index_computer_vision/ViewController.swift
+++ ios_index_computer_vision/ios_index_computer_vision/ViewController.swift
@@ -27,6 +27,26 @@ class ViewController: NSViewController {
         } else {
             print("there is no window")
         }
+        let displayID = CGMainDisplayID()
+        if let imageRef = CGDisplayCreateImage(displayID){
+            let url = URL(string: "file:///Users/svgro/Documents/GitHub/index_computer_vision/test.jpg")!
+            //let url = URL(fileURLWithPath: "file:///Users/svgro/Documents/GitHub/index_computer_vision/test.jpg")
+            writeCGImage(imageRef, to: url)
+            url.absoluteString
+        }
+        
+//        let fileUrl = URL(string: "file:///Users/svgro/Documents/GitHub/index_computer_vision/test.jpg")!
+//        let displayID = CGMainDisplayID()
+//        let screenShot:CGImage = CGDisplayCreateImage(displayID)!
+//        let bitmapRep = NSBitmapImageRep(cgImage: screenShot)
+//        let jpegData = bitmapRep.representation(using: NSBitmapImageRep.FileType.jpeg, properties: [:])!
+//
+//
+//        do {
+//            try jpegData.write(to: fileUrl, options: .atomic)
+//        }
+//        catch {print("error: \(error)")}
+        
     }
 
     override var representedObject: Any? {
@@ -35,6 +55,13 @@ class ViewController: NSViewController {
         }
     }
 
+    
+    @discardableResult func writeCGImage(_ image: CGImage, to destinationURL: URL) -> Bool {
+        guard let destination = CGImageDestinationCreateWithURL(destinationURL as CFURL, kUTTypePNG, 1, nil) else { return false }
+        CGImageDestinationAddImage(destination, image, nil)
+        print("I think it worked")
+        return CGImageDestinationFinalize(destination)
+    }
 
 }
 
